cc5a7d99edd50842b20ab9cd9485530570804ec2
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-08-28 00:38:22 -0700

    build

---
+++ client/build/precache-manifest.8bf1a93e6c812a0164024abf7f0b6bba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6d122c7d8b5aaa2a817fb020f5cdd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d88471b2bd70e794db30",
+    "url": "/static/css/main.09f7841b.chunk.css"
+  },
+  {
+    "revision": "998a442a5edeeefde6f8",
+    "url": "/static/js/2.17b998f6.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.17b998f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88471b2bd70e794db30",
+    "url": "/static/js/main.779ac431.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "a6cd4c512234b8b546bf806d19cbba5c",
+    "url": "/static/media/spinner.a6cd4c51.gif"
+  }
+]);
